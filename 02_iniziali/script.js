cf0dd1cfd98be7a69d33de115f162d43189383fb
danirando <danielerando@tiscali.it>
2025-04-08 15:44:58 +0200

    completato 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,34 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function iniziali() {
+  const lettereIniziali = [];
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    lettereIniziali.push(name[0]);
+  }
+  return lettereIniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+iniziali();
+console.log(iniziali());
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+// uso arrow function
+
+const inizialiArrow = () => {
+  const lettereInizialiArrow = [];
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    lettereInizialiArrow.push(name[0]);
+  }
+  return lettereInizialiArrow;
+};
+
+inizialiArrow();
+console.log(inizialiArrow());
