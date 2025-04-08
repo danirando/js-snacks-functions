1c1459f68c4d7556ca930bfa29a287a9b5ac3fef
danirando <danielerando@tiscali.it>
2025-04-08 15:55:32 +0200

    completato 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,40 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function inizialeA() {
+  const iniziale = "A";
+  let nomiConInizialeA = [];
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    if (name[0] === iniziale) {
+      nomiConInizialeA.push(name);
+    }
+  }
+  return nomiConInizialeA;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+inizialeA();
 
+console.log(inizialeA());
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+// uso arrow function
+
+const inizialeAArrow = () => {
+  const iniziale = "A";
+  let nomiConInizialeA = [];
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    if (name[0] === iniziale) {
+      nomiConInizialeA.push(name);
+    }
+  }
+  return nomiConInizialeA;
+};
+
+console.log(inizialeAArrow());
