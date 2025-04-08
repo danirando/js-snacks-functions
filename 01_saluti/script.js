eb61608566ba77eee556814719468c80df3a0ea2
danirando <danielerando@tiscali.it>
2025-04-08 15:16:01 +0200

    completato 1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = "Mario";
 
 // Dichiara la funzione qui.
 
+function saluto() {
+  let ciao = `Ciao ${userName}`;
+  return ciao;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
-
+saluto();
+console.log(saluto());
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+// uso arrow function
+
+const salutoArrow = () => {
+  let ciaoArrow = `Ciao ${userName}`;
+  return ciaoArrow;
+};
+
+console.log(salutoArrow());
