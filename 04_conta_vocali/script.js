3e3aba6b8b577a735c762da564f0d7dae422e903
danirando <danielerando@tiscali.it>
2025-04-08 16:05:05 +0200

    completato 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,51 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+function conteggioVocali() {
+  let quanteVocali = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (
+      word[i] === "a" ||
+      word[i] === "e" ||
+      word[i] === "i" ||
+      word[i] === "o" ||
+      word[i] === "u"
+    ) {
+      quanteVocali = quanteVocali + 1;
+    }
+  }
+  return quanteVocali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+conteggioVocali();
 
+console.log(`Le vocali sono: ${conteggioVocali()}`);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+// uso arrow function
+
+const conteggioVocaliArrow = () => {
+  let quanteVocali = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (
+      word[i] === "a" ||
+      word[i] === "e" ||
+      word[i] === "i" ||
+      word[i] === "o" ||
+      word[i] === "u"
+    ) {
+      quanteVocali = quanteVocali + 1;
+    }
+  }
+  return quanteVocali;
+};
+
+conteggioVocaliArrow();
+
+console.log(`Le vocali sono: ${conteggioVocaliArrow()}`);
